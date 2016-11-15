d204a03c9f6f0c198009789078bea8d542d1739f
HasmukhGondaliya <gondaliyaheet@gmail.com>
2016-11-15 14:56:35 +0530

    update log

--- login_ctrl.php
+++ login_ctrl.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 require_once APPPATH . '/libraries/REST_Controller.php';
 
-class Login_ctrl extends REST_Controller     //rest server in library
+class Login_ctrl extends CI_Controller
 {
     public function __construct()
     {
@@ -11,79 +11,44 @@ class Login_ctrl extends REST_Controller     //rest server in library
         $this->load->model('login_model');
         $this->load->model('reg_model');
         $this->load->library('session');
-    }
-
-    public function index_get()
-    {
-        $abc = $this->login_model->get();
-
-        if (!is_null($abc)) {
-            $this->response(array('response' => $abc), 200);
-        } else {
-            $this->response(array('error' => 'No User Found'), 404);
-        }
-    }
-
-    public function find_get($id)
-    {
-        if (!$id) {
-            $this->response(null, 400);
-        }
-        $city = $this->login_model->get($id);
-
-        if (!is_null($city)) {
-            $this->response(array('response' => $city), 200);
-        } else {
-            $this->response(array('error' => 'No User Found'), 404);
-        }
+        log_info($this->input->ip_address());
+		log_info('login_ctrl Controller Class Initialized');
+		log_debug('login_ctrl Controller Class');
+		
     }
 
     public function index(){
-        $this->ProfileApi();
+        $this->LoginApi();
+        
     }
-//profile function
-    public function ProfileApi(){
-        $result = $this->reg_model->ProfileApi();
-        echo json_encode($result);
-    }
-//login function
     public function LoginApi()
     {
-        $username = $this->input->post('username');
+        $user_name = $this->input->post('user_name');
         $password = $this->input->post('password');
-        $result = $this->login_model->LoginApi($username, $password);
-        if ($result){
+        if ($this->login_model->LoginApi($user_name, $password)){
             echo "Login Successfull";
-            $this->session->set_userdata('name',$username);
-            echo json_encode($result);
+			
+            $this->session->set_userdata('name',$user_name);
+    //        echo json_encode($result);
         } else {
-            echo "Login Failed Please try again...!";
-            echo json_encode($result);
+			log_error('Invalid Username and Password');
+                echo "{
+status: 400,
+kind: \"Bad Request\",
+problems: [
+{
+    code: \"400\",
+    title: \"Bad Request\",
+    message: \"You should enter valid username or password\",
+}
+}";
+            //echo json_encode($result);
+           
         }
-       
-    }
-//logout function
-    public function LogoutApi(){
-        $sess_array = array('username' => '');
-        $this->session->unset_userdata('logged_in', $sess_array);
-        echo "Logout Successfull..!";
-    }
-//forgot password function
-    public function Fpass()
-    {
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
-        $result = $this->login_model->Fpass($username);
-        if($result){
-        
-            if($this->login_model->Fpass1($username,$password)){
-                echo "Password Changed..!";
-            }else{
-                echo "Oops..!";
-            }
-        }else{
-            echo "No Username Found..!";
-        }
-       
-    }
+        //echo json_encode($result);
+		
+		
+	}
+
+
 }
